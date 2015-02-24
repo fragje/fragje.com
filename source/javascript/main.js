9a9f1b50891cee9b4d5244babe7c626781e1b4d9
Frank Gjertsen <frank.gjertsen@gmail.com>
2015-02-24 22:48:01 +0100

    Include old javascript

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -1,1 +1,10 @@
-// Custom javascript
+(function() {
+  // Toggle main nav
+  var $navBar = $('nav.nav-main');
+
+  $('a.toggle-nav').on('click', function(e) {
+    e.preventDefault();
+    $navBar.slideToggle(200);
+    $(this).toggleClass('active');
+  })
+})();
