c075e61aa2fb458427abc816a027dfd0cae51103
Frank Gjertsen <frank.gjertsen@gmail.com>
2015-03-01 23:14:15 +0100

    Update 2015-03-01T22:14:13.543Z

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+(function() {
+  // Toggle main nav
+  var $navBar = $('nav.nav-main');
+
+  $('a.toggle-nav').on('click', function(e) {
+    e.preventDefault();
+    $navBar.slideToggle(200);
+    $(this).toggleClass('active');
+  });
+})();
